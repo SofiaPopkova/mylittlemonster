38ac66c7293fbc959dd3b7ea13ae8d7cff85f6c3
Ruslan Zhuba <r.zhuba@12storeez.com>
2021-05-12 14:07:41 +0300

    начало

---
+++ lol.sql
@@ -0,0 +1,8 @@
+create table lol (joke varchar(50), stand_up varchar(30), amount int);
+
+insert into lol (joke, stand_up, amount)
+values ('dont_funny', 'comedy', 3);
+
+select (joke, stand_up, amount)
+from lol;
+
