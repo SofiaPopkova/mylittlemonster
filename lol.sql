f460beff09dbf5613d762cf3a44c69cb9d391f1f
Ruslan Zhuba <r.zhuba@12storeez.com>
2021-05-12 14:27:37 +0300

    test commit

--- lol.sql
+++ lol.sql
@@ -1,10 +1,1 @@
-create table lol (joke varchar(50), stand_up varchar(30), amount int);
-
-insert into lol (joke, stand_up, amount)
-values ('dont_funny', 'comedy', 3);
-
-select (joke, stand_up, amount)
-from lol;
-
-
-create table kek (memes varchar(20), funny_gif int, date_)
+# supper comment
